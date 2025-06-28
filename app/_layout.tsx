ef3dc349aef87b51798fff29d39d033c0924eef3
Scyllo <moondark.scyllo@gmail.com>
2025-06-28 19:56:16 +0200

    feat: Setup after the expo app creation

---
+++ app/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from "expo-router";
+import { useColorScheme } from "react-native";
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme()
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack />
+    </ThemeProvider>
+  );
+}
