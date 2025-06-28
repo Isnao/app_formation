3aaf9b7d3974ade86d6be63925fa850a3bf2b4f4
Scyllo <moondark.scyllo@gmail.com>
2025-06-28 23:46:43 +0200

    chore: update prettier and eslint config + fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,10 @@
-import { Stack } from "expo-router";
-import { useColorScheme } from "react-native";
-import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { Stack } from 'expo-router'
+import { useColorScheme } from 'react-native'
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native'
 
 export default function RootLayout() {
   const colorScheme = useColorScheme()
@@ -8,5 +12,5 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack />
     </ThemeProvider>
-  );
+  )
 }
